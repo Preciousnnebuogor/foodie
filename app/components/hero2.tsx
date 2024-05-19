26f04b0a4aeed9181c811c2118d1c182f02a46d3
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-19 16:52:21 +0100

    animation working

--- app/components/hero2.tsx
+++ app/components/hero2.tsx
@@ -4,13 +4,16 @@ import {animate, motion} from "framer-motion"
 export default function Hero2(){
     return <div className={`flex overflow-x-hidden items-center justify-between`}>
        <motion.section
-       className={"flex items-center justify-between"}
-       initial={{ y: 100, opacity: 0 }}
-       animate={{ y: 0, opacity: 1 }}
-       transition={{ ease: "easeout", duration: 1, delay:0.2 }}
+       className={"h-20 w-20"}
+       animate={{
+        scale:[1,2,2,1],
+        rotate:[0,90,90,0],
+        borderRadius:["10%", "10%", "50%", "10%"],
+       }}
+       transition={{duration: 5, ease:"easeInOut", repeat:Infinity,repeatDelay:1}}
        >
-       <img src={ImageApp.logo} className={"h-8 w-8"} />
-       <img src={ImageApp.logo} className={"h-8 w-8"} />
+       <img src={ImageApp.logo} />
+       <img src={ImageApp.logo} />
        
 
         
