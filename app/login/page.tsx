1857525a0230b93f77fe901e9c6d4eb46e5975e4
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-27 21:43:18 +0100

    added type on input

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,32 @@
 "use client"
-export default function Login(){
-    return <div className={`mt-40`}>
-        <div className={` flex flex-col items-center justify-center`}>
-            <input placeholder="email" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
-            uppercase text-xl md:self-start my-5`}/>
-            <input placeholder="password" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
-            uppercase text-xl md:self-start my-5`}/>
-            <input placeholder="number" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
-            uppercase text-xl md:self-start my-5`}/>
-            <input placeholder="name" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
-            uppercase text-xl md:self-start my-5`}/>
-        </div>
+export default function Login() {
+  return (
+    <div className={`mt-40`}>
+      <div className={` flex flex-col items-center justify-center`}>
+        <input
+          placeholder="email"
+          type="email"
+          className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}
+        />
+        <input
+          placeholder="number"
+          type="number"
+          className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}
+        />
+        <input
+          placeholder="pin"
+          className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}
+        />
+      </div>
     </div>
+  )
 }
