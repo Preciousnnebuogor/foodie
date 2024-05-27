3ca647a6abc685171829692d6474846793b6acac
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-27 21:33:20 +0100

    added input field

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 export default function Login(){
-    return <div>
-        <div>
-            <input placeholder="email"/>
-            <input placeholder="password"/>
-            <input placeholder="number"/>
-            <input placeholder="name"/>
+    return <div className={`mt-40`}>
+        <div className={` flex flex-col items-center justify-center`}>
+            <input placeholder="email" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}/>
+            <input placeholder="password" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}/>
+            <input placeholder="number" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}/>
+            <input placeholder="name" className={`mb-4 rounded-full bg-gradient-to-r from-orange-100 to bg-orange-400 py-4 px-8 
+            uppercase text-xl md:self-start my-5`}/>
         </div>
     </div>
 }
