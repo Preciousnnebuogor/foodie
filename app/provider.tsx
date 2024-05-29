f068d59020fb79afabf1332da3b4b556e6f630f2
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-29 16:59:42 +0100

    added function.ts

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,26 +2,29 @@
 
 import { ReactNode } from "react"
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { WagmiProvider, http } from "wagmi"
 import { celo, celoAlfajores } from "wagmi/chains"
 
+const queryClient = new QueryClient()
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string
 const rainbowConfig = getDefaultConfig({
-    appName: "Peerie",
-    projectId: projectId,
-    chains: [celo, celoAlfajores],
-    ssr: true, // If your dApp uses server side rendering (SSR)
-    transports: {
-      [celo.id]: http(),
-      [celoAlfajores.id]: http(),
-    },
-  })
+  appName: "Peerie",
+  projectId: projectId,
+  chains: [celo, celoAlfajores],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+  transports: {
+    [celo.id]: http(),
+    [celoAlfajores.id]: http(),
+  },
+})
 
 export default function Provider(props: { children: ReactNode }) {
   return (
     <WagmiProvider config={rainbowConfig}>
-      <RainbowKitProvider>
-        {props.children}
-        </RainbowKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{props.children}</RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   )
 }
