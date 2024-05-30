9b77f5fac22454418cac210f8cb3b49e8933fa27
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-05-30 23:03:30 +0100

    adjusted hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 export default function Hero() {
   return (
-    <div className={"flex justify-center items-center h-screen"}>
+    <div className={"flex justify-center items-center"}>
       <div className={" mx-auto p-5 h-full w-[80%]"}>
         <div className={"mt-5 md:mt-20 md:flex md:flex-row "}>
           <div
@@ -15,7 +15,7 @@ export default function Hero() {
             }>
            <h2
               className={
-                "text-4xl md:text-5xl text-center md:text-left md:self-start mt-20 md:mt-0"
+                "text-5xl text-center md:text-left md:self-start mt-20 md:mt-0"
               }
             >
               Special delicacy for more
